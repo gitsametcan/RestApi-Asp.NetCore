a1234c69551f25d76ea40b54586544a9fd42ff30
Samet CAN <cansamet1223@gmail.com>
2023-07-14 19:18:02 +0300

    Starting

---
+++ BackendWorks/Models/First.cs
@@ -0,0 +1,13 @@
+﻿namespace BackendWorks.Models
+{
+    public class First
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ BackendWorks/Models/FirstContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BackendWorks.Models
+{
+    public class FirstContext : DbContext
+    {
+
+        public FirstContext(DbContextOptions<FirstContext> options)
+            : base(options)
+        { 
+        }
+
+        DbSet<First> Firsts { get; set; } = null!;
+    }
+}
